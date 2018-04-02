71e0518ce934822d6145c0c365e4ed13df4bc51c
albertagung <albiardtya@gmail.com>
2018-04-03 01:42:03 +0700

    adding API request to update loginState schema mongodb

--- function_js/logOut.js
+++ function_js/logOut.js
@@ -3,6 +3,9 @@ $(document).ready(() => {
 	// Define sign out url
 	const urlSignOut = 'http://localhost:3000/auth/signOut'
 
+	// Define update login state url
+	const urlUpdateLoginState = `http://localhost:3000/loginState/5ac27641eb4fab1b5c9eab87`
+
 	// When button log out pressed
 	$('#btnLogOut').click( async (e) => {
 		await e.preventDefault()
@@ -11,9 +14,17 @@ $(document).ready(() => {
 		// Sign out user
 		await axios.get(urlSignOut)
 		.then((response) => {
-			console.log(response.data)
-			// Load to login page
-			window.location.replace('page-login.html')
+			// Make login state database null prior to log out
+			let objUser = {
+				firstName: null,
+				lastName: null,
+				email: null
+			}
+			axios.put(urlUpdateLoginState, objUser)
+			.then((response) => {
+				// Load to login page
+				window.location.replace('page-login.html')
+			})
 		})
 	})
 
--- function_js/pageCheck.js
+++ function_js/pageCheck.js
@@ -1,11 +1,21 @@
 $(document).ready(() => {
 
+	// Define update login state url
+	const urlUpdateLoginState = `http://localhost:3000/loginState/5ac27641eb4fab1b5c9eab87`
+
 	// Check credentials
 	let checkCredentialsUrl = 'http://localhost:3000/auth/check'
 	axios.get(checkCredentialsUrl)
 	.then((response) => {
 		// Set logged in user data in localStorage
 		localStorage.setItem('dataUsers', JSON.stringify(response.data))
+		// Define obj user for database
+		let objUser = {
+			firstName: response.data[0].firstName,
+			lastName: response.data[0].lastName,
+			email: response.data[0].email
+		}
+		axios.put(urlUpdateLoginState, objUser)
 	})
 	.catch((err) => {
 		swal("Alert!", "Please log in first!", "warning").then(() => {
